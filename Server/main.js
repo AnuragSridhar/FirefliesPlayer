548f8fd38447e9666446d5ab628d7f8878335043
AnuragSridhar <anurag.sridhar23@gmail.com>
2018-05-29 22:06:10 -0700

    HEROKU

--- Server/main.js
+++ Server/main.js
@@ -2,50 +2,7 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-//const cloudSpeech = require('@google-cloud/speech');
-//const ffmpeg = require('ffmpeg');
 
-//const client = new cloudSpeech.SpeechClient();
-
-
-/*app.get('/speechtesting' , function(req, res) {
-
-  console.log('Speech Api Running...');
-
-  const file = fs.readFileSync('test_files/audio/speech.mp3');
-  const audioBytes = file.toString('base64');
-
-  const audio = {
-  content: audioBytes,
-};
-const config = {
-  //encoding: 'LINEAR16',
-  encoding : '',
-  sampleRateHertz : 44100,
-  languageCode : 'en-US',
-};
-const request = {
-  audio: audio,
-  config: config,
-};
-
-// Detects speech in the audio file
-client
-  .recognize(request)
-  .then(data => {
-    const response = data[0];
-    console.log(response.results);
-    const transcription = response.results
-      .map(result => result.alternatives[0].transcript)
-      .join('\n');
-    console.log(`Transcription: ${transcription}`);
-  })
-  .catch(err => {
-    console.error('ERROR:', err);
-  });
-
-});
-*/
 app.get('/getnames', function(req, res) {
 
   var filepath = 'test_files/audio'
